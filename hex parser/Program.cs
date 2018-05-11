f4209628879089e9e4dd520dbdacedbea455f993
Kukac <thekukac9@gmail.com>
2018-05-11 16:21:10 +0200

    update for worse, not universal enough, maybe one day

--- hex parser/Program.cs
+++ hex parser/Program.cs
@@ -11,20 +11,24 @@ namespace hex_parser
     {
         static void Main(string[] args)   
         {
-            string line;
-            string[] hexes;
             int lines = 0;
-            using (StreamReader sr = new StreamReader("chernobyl_memory_dump.txt"))
+            using (StreamReader sr = new StreamReader("sent_hex.txt"))
             {
                 while (!sr.EndOfStream)
                 {
-                    line = sr.ReadLine();
-                    hexes = line.Split(' ');
+                    string line = sr.ReadLine();
+                    string[] hexes = line.Split(' ');
 
                     using (StreamWriter sw = new StreamWriter("hex.txt", append: true))
                     {
                         for (int i = 0; hexes.Length > i; i++)
-                            sw.Write("0x" + hexes[i][0] + hexes[i][1] + ", " + "0x" + hexes[i][2] + hexes[i][3] + ", ");
+                        {
+                            if ((hexes[i] == ""))
+                            {
+                                break;
+                            }
+                            sw.Write("0x" + hexes[i][0] + hexes[i][1]);// + ", " + "0x" + hexes[i][2] + hexes[i][3] + ", ");
+                        }
                         sw.WriteLine();
                         Console.WriteLine("Amount of line(s) processed: {0}", ++lines);
                     }
